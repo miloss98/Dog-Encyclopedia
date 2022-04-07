3b1c31ba3403ad74619141edd4be0566b235841c
stojanovic98m <stojanovic98m@gmail.com>
2022-04-08 00:52:03 +0200

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SharedLayout from "./Components/SharedLayout";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Dog from "./Pages/Dog";
+import Error from "./Pages/Error";
 
 const App = () => {
   return (
-    <div>
-      <Home />
+    <div className="all">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/dog" element={<Dog />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };

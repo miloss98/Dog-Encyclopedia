fa1ffeae6ebc5705530f16c80704a06fa4f82a40
stojanovic98m <stojanovic98m@gmail.com>
2022-04-09 00:14:40 +0200

    error page changes

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./page_styles/error.css";
 
 const Error = () => {
-  return <div>Error page</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="error-container">
+      <div className="go-back-div">
+        <button id="go-back-btn" onClick={() => navigate(-1)}>
+          <img
+            id="arrow-go-back"
+            src="./../../images/go-back.svg"
+            alt="Go back?"
+          />
+        </button>{" "}
+        <h4> Go back? </h4>
+      </div>
+      <h1 id="not-found"> The page you requested does not exist!</h1>
+    </div>
+  );
 };
 
 export default Error;

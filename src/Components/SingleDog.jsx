c3f10a0360eb970f86eb5261d20ac3873e0e28d2
stojanovic98m <stojanovic98m@gmail.com>
2022-04-11 00:30:25 +0200

    single page styles

--- src/Components/SingleDog.jsx
+++ src/Components/SingleDog.jsx
@@ -3,6 +3,7 @@ import Loading from "./Loading";
 import Home from "./../Pages/Home";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import goBack from "./../images/go-back.svg";
 
 const SingleDog = () => {
   const url = "https://api.thedogapi.com/v1/breeds/";
@@ -29,38 +30,57 @@ const SingleDog = () => {
     return <Loading />;
   }
   return (
-    <>
-      <section>
-        <h1>Single dog page {data.dogId} </h1>
-
-        <article>
+    <section className="single-dog-container">
+      <nav>
+        <h1> {data.name} </h1>
+        <Link to="/" element={<Home />}>
+          {" "}
+          <button className="back-to-list">
+            {" "}
+            <img id="arrow-go-back" src={goBack} alt="Go back?" />
+          </button>
+        </Link>
+      </nav>
+      <article className="dog-info-container">
+        <div className="dog-img-div">
           <img
             src={`${imageUrl}${data.reference_image_id}.jpg`}
             alt={data.name}
           />
-          <h1> {data.name} </h1>
-          <p> {data.life_span} </p>
-          <p>{data.temperament} </p>
-          <p> Bred for: {data.bred_for || "/"} </p>
-          <p> Breed group: {data.breed_group || "/"} </p>
-          <p> Origin country: {data.origin || "/"} </p>
+        </div>
+        <section className="details">
+          <p id="life-span">
+            <span id="keyword">Life span:</span> {data.life_span}
+          </p>
+          <p id="temperament">
+            <span id="keyword">Temperament:</span> {data.temperament}
+          </p>
+          <p id="breed">
+            <span id="keyword">Bred for:</span> {data.bred_for || "/"}
+          </p>
+          <p id="breed">
+            <span id="keyword">Breed group:</span> {data.breed_group || "/"}
+          </p>
           <p>
-            <span>Height:</span> imperial: {data.height?.imperial || "/"}{" "}
-            <br></br>
+            <span id="keyword">Origin country:</span> {data.origin || "/"}
+          </p>
+          <p>
+            <span id="keyword">
+              Height: <br></br>
+            </span>{" "}
+            imperial: {data.height?.imperial || "/"} <br></br>
             metric: {data.height?.metric}
           </p>
           <p>
-            <span>Weight:</span> imperial: {data.weight?.imperial || "/"}{" "}
-            <br></br>
+            <span id="keyword">
+              Weight: <br></br>
+            </span>{" "}
+            imperial: {data.weight?.imperial || "/"} <br></br>
             metric: {data.weight?.metric}
           </p>
-          <Link to="/" element={<Home />}>
-            {" "}
-            Back to list
-          </Link>
-        </article>
-      </section>
-    </>
+        </section>
+      </article>
+    </section>
   );
 };
 

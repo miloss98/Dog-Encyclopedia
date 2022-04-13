4f4c9d7b098bd4c0e4c9f07d9347749d3b3bee65
stojanovic98m <stojanovic98m@gmail.com>
2022-04-13 02:10:34 +0200

    all done

--- src/Components/AllDogs.jsx
+++ src/Components/AllDogs.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./component_styles/allDogs.css";
 import searchIcon from "./../images/search-icon.svg";
 import { RingLoader } from "react-spinners";
+
+//api
+const url = "https://api.thedogapi.com/v1/breeds/";
 
 const AllDogs = () => {
   const [loading, setLoading] = useState(false);
@@ -11,8 +14,16 @@ const AllDogs = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [search, setSearch] = useState("");
 
-  const url = "https://api.thedogapi.com/v1/breeds/";
-
+  //Search values
+  const searchValue = useRef("");
+  const searchDogs = () => {
+    setSearch(searchValue.current.value);
+  };
+  const handleRefresh = () => {
+    fetchData();
+    console.log(data);
+  };
+  //Data fetching on initial render
   const fetchData = async () => {
     setLoading(true);
     try {
@@ -26,11 +37,11 @@ const AllDogs = () => {
       setLoading(false);
     } catch (error) {}
   };
-
   useEffect(() => {
     fetchData();
   }, []);
 
+  // useEff for search/query
   useEffect(() => {
     setFilteredData(
       data.filter((dog) =>
@@ -46,9 +57,34 @@ const AllDogs = () => {
       </div>
     );
   }
-  //if (data.length < 1) {
-  //return <h1> No dogs matched your search criteria</h1>;
-  //}
+  if (filteredData == "" && filteredData.length < 1) {
+    return (
+      <div className="no-dogs-container">
+        <p id="no-dogs">
+          {" "}
+          No dogs matched your criteria :(
+          <br></br>
+          <h4> Please, try again. </h4>
+        </p>
+
+        <section className="search-bar-container">
+          <div className="search-div">
+            <img id="search-icon" src={searchIcon} alt="search icon" />
+            <input
+              className="input-field"
+              type="text"
+              placeholder="e.g. Husky"
+              ref={searchValue}
+            ></input>
+            <button className="search-btn" type="click" onClick={searchDogs}>
+              Search
+            </button>
+          </div>
+        </section>
+      </div>
+    );
+  }
+
   return (
     <div className="all-dogs-container">
       <header className="heading-div">
@@ -60,11 +96,9 @@ const AllDogs = () => {
               className="input-field"
               type="text"
               placeholder="e.g. Husky"
-              onChange={(e) => {
-                setSearch(e.target.value);
-              }}
+              ref={searchValue}
             ></input>
-            <button className="search-btn" type="click">
+            <button className="search-btn" type="click" onClick={searchDogs}>
               Search
             </button>
           </div>

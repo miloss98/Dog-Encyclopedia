5c250ee0de2501b1b44834874876ebef9ea70e75
stojanovic98m <stojanovic98m@gmail.com>
2022-05-22 20:39:56 +0200

    removed trash

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,16 @@ import { Home, About, Error, SingleDog } from "./Pages/index";
 
 const App = () => {
   return (
-    <div className="all">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/dogs/:dogId" element={<SingleDog />} />
-            <Route path="*" element={<Error />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dogs/:dogId" element={<SingleDog />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
